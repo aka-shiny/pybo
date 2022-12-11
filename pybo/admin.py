f120d32840a848e743dbdec0465ae3a35ab3bccd
shiny <aka.shiny.dev@gmail.com>
2022-12-11 13:44:50 +0900

    pybo admmin 등록

--- pybo/admin.py
+++ pybo/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Question
 
 
-class QuestionAdmin(admin.ModelAdmin):
+class QuestionAdmin(admin.ModelAdmin[Question]):
     search_fields = ['subject']
 
 
