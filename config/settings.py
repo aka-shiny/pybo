ce92526b451e98b6c93c8572760f0e86898939ba
shiny <aka.shiny.dev@gmail.com>
2022-12-18 20:18:33 +0900

    pybo, 로그인, 로그아웃, 회원가입 기능

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "pybo.apps.PyboConfig",
+    "common.apps.CommonConfig",
 ]
 
 MIDDLEWARE = [
@@ -99,3 +100,9 @@ STATICFILES_DIRS = [
 ]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# 로그인 성공후 이동하는 URL
+LOGIN_REDIRECT_URL = "/"
+
+# 로그아웃시 이동하는 URL
+LOGOUT_REDIRECT_URL = "/"
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from pybo.views import QuestionListView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("pybo/", include("pybo.urls")),
+    path("common/", include("common.urls")),
+    path("", QuestionListView.as_view(), name="index"),
 ]
